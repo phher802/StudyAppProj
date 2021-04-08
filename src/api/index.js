d2c902d2da33e464e5bf6e876f08a46e19fa38be
phher802 <pher024@gmail.com>
2021-04-07 22:00:08 -0500

    Add api folder and index.js file to connect backend to react app

---
+++ src/api/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/api',
+});
+
+export const insertSubject = payload => api.post('/subject', payload);
+export const getAllSubjects = () => api.get('/subjects');
+export const updateSubjectById = (id, payload) => api.put('/subject/${id}', payload);
+export const deleteSubjectById = id => api.delete('/subject/${id}');
+export const getSubjectByID = id => api.get('/subject/${id}');
+
+const apis = {
+    insertSubject,
+    getAllSubjects,
+    updateSubjectById,
+    deleteSubjectById,
+    getSubjectByID,
+}
+
+export default api;
